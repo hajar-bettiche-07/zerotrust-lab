953f228c09737ec07562e41db82bf134704d442f
hajar-bettiche-07 <bettichehajar@gmail.com>
2026-08-10 00:43:03 +0100

    feat: create virtual network and subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-zerotrust-lab"
   location = "westeurope"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-zerotrust-lab"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-lab"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
