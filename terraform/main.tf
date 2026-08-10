0686e78befb99fb89d76a0ee4e0381aa123b5382
hajar-bettiche-07 <bettichehajar@gmail.com>
2026-08-11 00:07:08 +0100

    feat: provision Azure virtual machine

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Déclaration du provider requis
 terraform {
     required_providers {
         azurerm= {
@@ -7,14 +8,20 @@ terraform {
     }
 }
 
+# Activation du provider
+
 provider "azurerm" {
     features {}
 }
+
+# première ressource : le Resource Group
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-zerotrust-lab"
   location = "westeurope"
 }
+
+# deuxième ressource : azurerm_virtual_network
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-zerotrust-lab"
@@ -23,12 +30,16 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Type de ressource : azurerm_subnet
+
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-lab"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# Network Security Group (NSG)
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-zerotrust-lab"
@@ -45,5 +56,67 @@ resource "azurerm_network_security_group" "nsg" {
     destination_port_range     = "22"
     source_address_prefix      = var.my_ip
     destination_address_prefix = "*"
+  }
+}
+
+# créationn de la ressource azurerm_public_ip
+
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-zerotrust-lab"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# azurerm_network_interface (NIC)
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-zerotrust-lab"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+
+# azurerm_network_interface_security_group_association 
+
+resource "azurerm_network_interface_security_group_association" "assoc" {
+  network_interface_id     = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# azurerm_linux_virtual_machine
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-zerotrust-lab"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "my_ip" {
   description = "41.249.82.253/32"
   type        = string
 }
+
+variable "ssh_public_key_path" {
+  description = "Chemin vers ta clé SSH publique"
+  type        = string
+  default     = "C:/Users/Hajar Bettiche/.ssh/zerotrust-lab.pub"
+}
